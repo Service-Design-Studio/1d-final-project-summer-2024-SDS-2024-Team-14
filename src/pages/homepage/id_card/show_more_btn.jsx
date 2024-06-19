e5602e4af71e5a4b6cbb6f226fd83a37dd95efe0
ElliotMonde <elliotphua@gmail.com>
2024-06-19 13:36:22 +0800

    add state hook for id card button

--- src/pages/homepage/id_card/show_more_btn.jsx
+++ src/pages/homepage/id_card/show_more_btn.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react';
 import Image from 'next/image'
-export default function ShowBtn() {
-     const [isHovering, setIsHovering] = useState(false);
+export default function ShowBtn(props) {
+    const [isHovering, setIsHovering] = useState(false);
     let onMouseEnter = () => setIsHovering(true);
     let onMouseLeave = () => setIsHovering(false);
+
+    const [isOpen, setIsOpen] = useState(false);
+    let onClick = () => setIsOpen(!isOpen);
     return (
         <div className="my-6">
-            <button className="btn-darkblue" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-                {isHovering ? <Image className="fill-current icon" width={120} height={0} src="/images/open_eye_blue.svg" alt="eye open icon" /> : <Image className="fill-current icon" width={120} height={0} src="/images/open_eye.svg" alt="eye open icon"/>}
-                <span className="btn-text">Show UNHCR Card</span>
+            <button className="btn-darkblue" onClick={onClick} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+                {isHovering ? <Image className="fill-current icon" width={120} height={0} src={isOpen ? "/images/close_eye_blue.svg" : "/images/open_eye_blue.svg"} alt="eye open icon" /> : <Image className="fill-current icon" width={120} height={0} src={isOpen ? "/images/close_eye.svg" : "/images/open_eye.svg"} alt="eye open icon" />}
+                <span className="btn-text">{isOpen ? "Hide" : "Show"} UNHCR Card</span>
             </button>
         </div>
     )
