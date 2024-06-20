3cb15743ce248f2775857b842920d959637e1190
ElliotMonde <elliotphua@gmail.com>
2024-06-20 22:21:30 +0800

    added axios get user to populate fields in id card

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,12 +1,32 @@
 import Features from "./features.jsx";
 import HomeHeader from "./home_header.jsx"
+import Loading from "../loading.jsx"
 import IdCard from "./id_card.jsx";
+import { useEffect, useState } from 'react';
+import axios from "axios";
+export default function Homepage(props) {
+    const [data, setData] = useState({});
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const { data: res } = await axios.get("https://gebirah-backend-2r6b52gguq-as.a.run.app/users/1");
+                setData(res);
+            } catch (error) {
+                console.error(error.message);
+            }
+            setLoading(false);
+        }
+        fetchData();
+    }, []);
 
-export default function Homepage() {
     return (
         <div className="flex flex-col  px-5 w-90 items-center">
-            <HomeHeader text={ "Home"} />
-            <IdCard />
-            <Features/>
+            {!props.loading && data != {} && <div><HomeHeader text={"Home"} />
+                <IdCard data={data} loading={loading} />
+                <Features /></div>}
+            {props.loading && data != {} && <Loading text={ "Loading..."} />}
+            {!props.loading && props.data == {} && <Loading text={ "500: Internal Error\nUnable to fetch user data" }/>}
         </div>);
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,13 @@
 import Head from 'next/head';
 import Homepage from "./homepage/homepage.jsx";
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react';
-import "../styles/globals.css"
-import axios from "axios";
-import Loading from './loading.jsx';
+import { useRouter } from 'next/router';
+import "../styles/globals.css";
 
 export default function Home() {
-
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState({});
   const router = useRouter();
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const { data: res } = await axios.get("https://gebirah-backend-2r6b52gguq-as.a.run.app/users/1");
-        setData(res);
-        console.log("console logging: ", res);
-      } catch (error) {
-        console.error(error.message);
-      }
-      setLoading(false);
-    }
-  fetchData();
-  }, []);
-
   return (
     <main className="flex flex-col align-middle min-w-minContent min-h-screen my-0 mx-0 bg-white transition-all-500">
-      {loading && <Loading/> }
-      {!loading && <Homepage />}
+      <Homepage/>
     </main>
   );
 }
