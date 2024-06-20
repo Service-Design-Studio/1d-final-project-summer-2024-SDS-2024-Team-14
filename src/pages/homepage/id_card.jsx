4863039923157134e558215cc6a6a69a482e0d66
Yang Si Jun <aersijun@gmail.com>
2024-06-20 13:04:06 +0800

    axios

--- src/pages/homepage/id_card.jsx
+++ src/pages/homepage/id_card.jsx
@@ -3,11 +3,12 @@ import ProfilePic from "./id_card/profile_pic"
 import PersonalInfo from "./id_card/personal_info"
 import { useState } from "react"
 import Image from "next/image"
+import axios from "axios";
 
 
 export default function IdCard() {
     const [isOpen, setIsOpen] = useState(false);
-    let onClick = () => {
+    let expandCard = () => {
         setIsOpen(!isOpen);
         // if (isOpen) {
         //     document.getElementById("qr").classList.remove("hide-card");
@@ -16,8 +17,11 @@ export default function IdCard() {
         //     document.getElementById("qr").classList.remove("open-card");
         //     document.getElementById("qr").classList.add("hide-card");
         // }
+        axios.get("https://gebirah-backend-2r6b52gguq-as.a.run.app/users")
+            .then(response => {
+                console.log(response.data)
+        })
     }
-
     return (
         <div className="id-card">
             <div className="block">
@@ -25,7 +29,7 @@ export default function IdCard() {
                     <ProfilePic />
                     <PersonalInfo />
                 </div>
-                <ShowBtn isOpen={isOpen} onClick={onClick} />
+                <ShowBtn isOpen={isOpen} onClick={expandCard} />
             </div>
             <div className="min-w-full text-darkblue" style={isOpen ? { visibility: "visible", height: "fit-content" } : { visibility: "hidden", height: "0" }}>
                 <span className="btn-text my-4">Scan For Documents</span>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Homepage from "./homepage/homepage.jsx";
 import "../styles/globals.css"
+import axios from "axios";
 export default function Home() {
   return (
     <main className="min-w-minContent min-h-screen bg-white">
