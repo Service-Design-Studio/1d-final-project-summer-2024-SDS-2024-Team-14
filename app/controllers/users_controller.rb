11a7de29cdbc217c5e240f10653945e73fc3d78e
Yang Si Jun <aersijun@gmail.com>
2024-06-21 03:01:50 +0800

    tet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         render json: @user, status: :ok
+        puts "test"
     end
 
     def create
