1ce5d264ce19f66e4ae32e11eb79740aed529cb6
ElliotMonde <elliotphua@gmail.com>
2024-06-21 01:27:32 +0800

    more info page placeholder

--- src/pages/detailed_info_page/infopage.jsx
+++ src/pages/detailed_info_page/infopage.jsx
@@ -1,9 +1,18 @@
 import IdCard from "../homepage/id_card";
+import PersonalInfo from "../homepage/id_card/personal_info";
+import ProfilePic from "../homepage/id_card/profile_pic";
+import ExtendedInfo from "./extended_info";
 
 export default function Info() {
     return (
-        <div>
-            <IdCard />
+        <div className="id-card">
+            <div className="block">
+                <div className="flex">
+                    <ProfilePic />
+                    <PersonalInfo />
+                </div>
+            </div>    
+            <ExtendedInfo />
         </div>
     )
 }
--- src/pages/homepage/id_card.jsx
+++ src/pages/homepage/id_card.jsx
@@ -2,8 +2,6 @@ import ShowBtn from "./id_card/show_more_btn"
 import ProfilePic from "./id_card/profile_pic"
 import PersonalInfo from "./id_card/personal_info"
 import { useState } from "react"
-import Image from "next/image"
-import axios from "axios";
 import { useQRCode } from 'next-qrcode';
 
     
@@ -32,20 +30,6 @@ export default function IdCard(props) {
             <div className="min-w-full text-darkblue" style={isOpen ? { visibility: "visible", height: "fit-content" } : { visibility: "hidden", height: "0" }}>
                 <span className="btn-text my-4">Scan For Documents</span>
                 <div className="mx-auto w-fit">
-                    <Canvas
-                        //TODO - change to dynamic parameter based on axios call
-                      text={'https://gebirah-aid-2r6b52gguq-as.a.run.app/users/1'}
-                      options={{
-                        errorCorrectionLevel: 'M',
-                        margin: 3,
-                        scale: 4,
-                        width: 200,
-                        color: {
-                          dark: '#000000',
-                          light: '#FFFFFF',
-                        },
-                      }}
-                    />
                 </div>
             </div>
         </div>)
