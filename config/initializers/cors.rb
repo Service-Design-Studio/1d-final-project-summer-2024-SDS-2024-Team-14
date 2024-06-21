670bf30525965555311130e3bbdf9bfe34055e93
Yang Si Jun <aersijun@gmail.com>
2024-06-22 01:21:10 +0800

    removed /?

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,8 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins "http://localhost:3000",
               "http://127.0.0.1:3000",
-              "https://gebirah-aid-2r6b52gguq-as.a.run.app/",
-              /\Ahttps:\/\/deploy-preview-\d{1,4}--yourwebsite\.domain\.app\z/
+              "https://gebirah-aid-2r6b52gguq-as.a.run.app"
   
       resource "*",
         headers: :any,
