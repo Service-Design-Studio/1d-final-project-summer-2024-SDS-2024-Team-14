3b9fcfd1785fe3981f4bfe4e9b3f58dd8d3dcdfa
ElliotMonde <elliotphua@gmail.com>
2024-07-09 20:32:01 +0800

    scanner ui mvp done

--- src/components/scanner/camera.js
+++ src/components/scanner/camera.js
@@ -1,12 +1,22 @@
 import Image from "next/image";
 import { Camera } from "react-camera-pro";
-import { useRef, useEffect } from 'react';
-export default function CameraView({cameraRef }) {
+import { useState } from 'react';
+import { Button } from "@mui/base"
+export default function CameraView({ cameraRef, cameraLoaded, setImage, camera }) {
+    const [hover, setHover] = useState("/images/camera_white.svg");
     return (
-        <div className="relative flex w-full lg:w-[500px] h-7/12 lg:h-[707px] items-center justify-center">
-            <span className="absolute bottom-14 z-20 p-1 text-white text-md sm:text-lg xsm:text-xl opacity-55 bg-default bg-opacity-60">Position document within boundary </span>
+        <div className="relative flex w-full h-7/12 items-center justify-center">
+            <span className="absolute top-14 z-20 p-1 text-white text-md sm:text-lg xsm:text-xl opacity-55 bg-default bg-opacity-60">Position document within boundary </span>
             <Image src="/images/A4_crosshairs.svg" className="absolute z-10 w-11/12" width={1} height={1} alt="Camera view" />
-            <Camera ref={cameraRef} facingMode="environment" aspectRatio={1 / 1.4142} />
+            <Button
+                className="btn-submit absolute z-40 w-fit bottom-5 rounded-full p-1"
+                onMouseEnter={() => setHover("/images/camera_darkblue.svg")}
+                onMouseLeave={() => setHover("/images/camera_white.svg")}
+                onClick={() => {
+                    if ({ cameraLoaded }) { setImage(cameraRef.current.takePhoto())}
+                }}
+            ><Image src={hover} className="w-fit" width={1} height={1} alt='Upload document'/></Button>
+            <Camera priority ref={cameraRef} facingMode="environment" aspectRatio={1 / 1.4142} />
         </div>
     )
 }
--- src/components/scanner/scanned_item.js
+++ src/components/scanner/scanned_item.js
@@ -2,8 +2,8 @@ import "../../styles/globals.css"
 import { Button } from "@mui/base"
 import Image from "next/image"
 export default function ScannedImage(props ) {
-    return (<div className="aspect-auto w-4/12 h-auto mr-3">
+    return (<div className="flex flex-col embla__slide items-center min-w-[100px max-w-[100px]  h-auto mr-3">
         <img src={props.src}/>
-        <Button className="text-red underline" onClick={() => props.remove(props.index)}><Image src="/images/cross.svg" className="w-fit h-auto mr-1" width={ 1} height={1} alt="Remove Item"/>Remove</Button>
+        <Button className="text-red underline flex flex-row items-center text-md xsm:text-xl" onClick={() => props.remove(props.index)}><Image src="/images/cross.svg" className="w-2/12 h-5 mr-1" width={ 1} height={1} alt="Remove Item"/>Remove</Button>
     </div>)
 }
