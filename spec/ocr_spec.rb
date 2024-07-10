0c95008892bfaedd5191b2cf05241fd65975f49a
Xephori <whatforeverla62@gmail.com>
2024-07-10 16:32:06 +0800

    wip ocr-spec

--- spec/ocr_spec.rb
+++ spec/ocr_spec.rb
@@ -1,15 +1,31 @@
 require "rails_helper"
-require_relative "../utils/ocr"
+require_relative "../app/utils/ocr"
 
 RSpec.describe "ocr", type: :request do
 
-    scenario "extract text from English PDF" do
+    scenario "successfully detect PDF files" do
         # get file path
-        engpdf = "/fixtures/mockengpdf.pdf"
-        response = ocr()
+        engpdf = "./fixtures/mockengpdf.pdf"
+        file = ocr(engpdf)
+        # expect(file["message"]).to eq("Supported pdf type")
+        expect(file).to_include(".pdf")
+    end
+
+    scenario "successfully extract text from English PDF" do
+        # get file path
+        engpdf = "./fixtures/mockengpdf.pdf"        
+        response = process_pdf(engpdf)
         # expect(response).to eq("Name, Text to speech, Phone number")
         puts response
     end
+
+    # scenario "failed to extract text from English PDF" do
+    #     # get file path
+    #     engpdf = "/fixtures/mockengpdf.pdf"
+    #     response = ocr(process_pdf(engpdf))
+    #     # expect(response).to eq("Name, Text to speech, Phone number")
+    #     puts response
+    # end
 
     # scenario "extract text from English PNG" do
         # engpng = "/fixtures/mockengpng.png"
