0871386e9f74b182af62872110e1787b81e4cbdc
Yang Si Jun <aersijun@gmail.com>
2024-07-10 22:54:36 +0800

    sijun-documents

--- src/components/document_manager/Accordion.js
+++ src/components/document_manager/Accordion.js
@@ -1,6 +1,8 @@
 import {Component, useState} from "react";
 import Image from "next/image";
 import tickIcon from "../../../public/images/tick_icon.svg"
+import crossIcon from "../../../public/images/cross_icon.svg"
+import exclamationIcon from "../../../public/images/exclamation_icon.svg"
 import accordionButton from "../../../public/images/accordion_button.svg"
 import Listing from "@/components/document_manager/dm_listing";
 
@@ -10,18 +12,25 @@ export default function Accordion(props){
         setAccordionOpen(!accordionOpen)
     }
     const data = props.documents;
-    console.log(data);
     return (
         <>
         <div className='bg-darkblue w-[95%] mx-auto'>
             <div className='py-4 flex flex-row items-center'>
                 <div className="ml-4 md:w-[3vw] w-[2vw]">
-                    <Image src={tickIcon} layout='responsive' alt="Tick Icon"/>
+                    {props.title === "Approved Documents" &&
+                        <Image src={tickIcon} layout='responsive' alt="Tick Icon"/>
+                    }
+                    {props.title === "Pending Documents" &&
+                        <Image src={exclamationIcon} layout='responsive' alt="Exclamation Icon"/>
+                    }
+                    {props.title === "Rejected Documents" &&
+                        <Image src={crossIcon} layout='responsive' alt="Cross Icon"/>
+                    }
                 </div>
                 <h1 className='ml-6 text-white font-bold md:text-[2vw] text-[3vw]'>
                     {props.title}
                 </h1>
-                <div className="w-[2vw] ml-auto mr-8">
+                <div className={`w-[2vw] ml-auto mr-8 ${accordionOpen ? 'rotate-180': ''}`}>
                     <Image src={accordionButton} layout="responsive" onClick={handleAccordion} alt="Accordion dropdown"/>
                 </div>
             </div>
