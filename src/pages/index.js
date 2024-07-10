153d53d7078c0d15a87904efee1bbef5b72fc03b
Yang Si Jun <aersijun@gmail.com>
2024-07-10 15:28:38 +0800

    fixed index/

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,17 @@
-// pages/index.js
-import React from 'react';
-import Router from 'next/router';
-// import DocumentManager from './document_manager';
 import Head from 'next/head';
-import Homepage from '../components/homepage/homepage';
-import '../styles/globals.css';
+import Homepage from "../components/homepage/homepage.js";
+import "../styles/globals.css";
+import { redirect } from 'next/navigation'
+import { getSession, SessionProvider} from "next-auth/react";
+import "../hooks/useAuth"
+import useAuth from "@/hooks/useAuth";
+// import {ReactNotifications} from "react-notifications-component";
 
-function Home() {
+export default function Home({ session }) {
+    useAuth();
   return (
-    <>
-      <Head>
-        <title>My App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Router>
-        <Homepage />
-      </Router>
-    </>
-  );
-}
-
-export default Home;
-
-
-
-
-/*export default function Home({ session }) {
-  return (
-    // <SessionProvider session={session}>
     <main className="flex flex-col align-middle min-h-screen my-0 mx-0 bg-white transition-all-500">
       <Homepage />
       </main>
-    // </SessionProvider>
   );
-}*/
+}
