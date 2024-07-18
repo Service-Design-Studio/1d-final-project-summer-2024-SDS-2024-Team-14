c06e57363eafcd374082a87886b872b05f80fbf2
Yang Si Jun <aersijun@gmail.com>
2024-07-18 23:31:30 +0800

    put abdul bari image

--- src/components/homepage/id_card/profile_pic.js
+++ src/components/homepage/id_card/profile_pic.js
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import avatar from "../../../../public/images/avatar_placeholder.png"
+
 export default function ProfilePic() {
     return (
-        <div className="profile-image">
-            <Image src="/images/Profile_pic_grp.svg" className='' width={160} height={160} alt="Profile photo"/>
-        </div>
+        <div className="overflow-hidden mt-2 w-3/12 rounded-2xl">
+            <Image className="rounded-2xl" src={avatar} alt="Profile photo"/>
+         </div>
     )
 }
