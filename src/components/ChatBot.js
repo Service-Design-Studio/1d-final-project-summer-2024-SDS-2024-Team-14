9e02382a28fcf2a783ff0234a2a9fede63bb8719
Yang Si Jun <aersijun@gmail.com>
2024-07-19 17:58:46 +0800

    chatbot responsiveness done

--- src/components/ChatBot.js
+++ src/components/ChatBot.js
@@ -40,10 +40,6 @@ export default function ChatBot() {
           receiveMessage(newMessage); // Simulate receiving a response
         }
     };
-    //     // id of the chat container ---------- ^^^
-    //     if (el) {
-    //       el.scrollTop = el.scrollHeight;
-    //     }
 
     const handleKeyPress = (e) => {
         if (e.key === 'Enter') {
@@ -61,34 +57,38 @@ export default function ChatBot() {
 
     return (
         <>
-            <div className="fixed bottom-10 right-10">
+            <div className="fixed sm:bottom-10 sm:right-10 bottom-4 right-4">
                 <button
-                    className={`cursor-pointer fixed bottom-10 right-10 ${chatState ? "hidden" : "inline-block"}`}
+                    className={`cursor-pointer fixed sm:bottom-10 sm:right-10 bottom-4 right-4 ${chatState ? "hidden" : "inline-block"}`}
                     onClick={() => setChatState(!chatState)}
                 >
                     <Image className="w-[12.5vw] sm:w-[10vw] lg:w-[3.5vw]" src={chatBot} />
                 </button>
-                <div className={`bg-white rounded-xl overflow-hidden w-[20vw] h-[30vw] drop-shadow-lg ${chatState ? "animate-grow" : "animate-shrink"}`}>
-                    <div className="bg-darkblue py-3 flex justify-between items-center">
-                        <div className="flex ml-4">
-                            <div className="w-[2.5vw]">
+                <div className={`bg-white rounded-xl overflow-hidden lg:w-[20vw] lg:h-[30vw] 
+                md:w-[55vw] md:h-[65vw] sm:w-[50vw] sm:h-[65vw] w-[65vw] h-[90vw] 
+                drop-shadow-lg ${chatState ? "animate-grow" : "animate-shrink"}`}>
+                    <div className="bg-darkblue sm:py-3 py-2 flex justify-between items-center">
+                        <div className="flex sm:ml-4 ml-2 items-center">
+                            {/*Avatar Bot*/}
+                            <div className="lg:w-[2.5vw] sm:w-[5.5vw] w-[7.5vw]">
                                 <Image src={helpBot} alt="Bot Picture"/>
                             </div>
-                            <div className="ml-4">
-                                <h1 className="text-[0.8vw] text-white">HelpBot</h1>
+                            <div className="sm:ml-4 ml-2">
+                                <h1 className="lg:text-[0.8vw] sm:text-[2.5vw] text-[3vw] text-white">HelpBot</h1>
                                 <div className="text-[#43EE7D] flex items-center space-x-1">
-                                    <Image className="w-[0.3vw] inline-block" src={greenDot} />
-                                    <span className="text-[0.8vw]">Online</span>
+                                    <Image className="lg:w-[0.3vw] sm:w-[0.8vw] w-[1.0vw] inline-block" src={greenDot} />
+                                    <span className="lg:text-[0.8vw] sm:text-[1.4vw] text-[2.6vw]">Online</span>
                                 </div>
                             </div>
                         </div>
                         <div className="mr-4">
-                            <Image onClick={() => setChatState(!chatState)} className="w-[12.5vw] sm:w-[10vw] lg:w-[1.3vw]" src={crossIcon} />
+                            <Image onClick={() => setChatState(!chatState)} className="w-[4.5vw] sm:w-[3.5vw] md:w-[4vw] lg:w-[1.3vw]" src={crossIcon} />
                         </div>
                     </div>
-                    <div id="chat-feed" className="overflow-y-scroll p-4 h-[21.5vw]">
-                        <div className="my-2 py-2 px-4 rounded-2xl clear-both bg-[#C6E8FA] float-left text-black">
-                            <h4 className="text-[0.8vw]">
+                    <div id="chat-feed" className="overflow-y-scroll px-4 sm:py-2 py-1
+                     lg:h-[21.5vw] md:h-[37.5vw] sm:h-[43vw] h-[62vw]">
+                        <div className="my-2 py-2 px-4 w-3/4 rounded-2xl clear-both bg-[#C6E8FA] float-left text-black">
+                            <h4 className="lg:text-[0.8vw] sm:text-[2vw] text-[2.5vw]">
                                 Hi, I am the Enable ID FAQ Chatbot. Ask me any questions about this website and I will try to answer them.
                                 You can ask questions such as "How do I upload a document" or "Please redirect me to the resource locator page".
                             </h4>
@@ -100,7 +100,7 @@ export default function ChatBot() {
                               message.sender === 'user' ? 'bg-darkblue float-right text-white' : 'bg-[#C6E8FA] float-left text-black'
                             }`}
                           >
-                              <h4 className="text-[0.8vw]">
+                              <h4 className="lg:text-[0.8vw] sm:text-[2vw] text-[2.5vw]">
                                 {message.text}
                               </h4>
                             </div>
@@ -109,13 +109,13 @@ export default function ChatBot() {
                     </div>
                     <div className="bg-white absolute bottom-0 w-full">
                         <hr className="border-t-2 border-[#B0B0B0]/50 mx-auto" />
-                        <div className="flex items-center mx-3 my-3">
-                            <input className="placeholder:text-slate-400 placeholder:text-[0.7vw]
-                            text-slate-400 text-[0.7vw] bg-[#E8EBF0] w-10/12 rounded-full
-                            py-2 px-5 mx-2 focus:outline-none focus:ring-sky-500 focus:ring-1"
+                        <div className="flex items-center mx-3 lg:my-3 my-4">
+                            <input className="placeholder:text-slate-400 placeholder:sm:text-[2vw] placeholder:text-[2.5vw] placeholder:lg:text-[0.7vw]
+                            text-slate-400 lg:text-[0.7vw] sm:text-[2vw] text-[2.5vw] bg-[#E8EBF0] w-10/12 rounded-full
+                            lg:py-2 sm:py-4 py-2 px-5 mx-2 focus:outline-none focus:ring-sky-500 focus:ring-1"
                                    placeholder="Type your message here ..." value={input} onChange={handleInputChange}
                             onKeyDown={handleKeyPress}/>
-                            <Image className="ml-2" onClick={handleSendMessage} src={send} alt="Send button"/>
+                            <Image className="ml-2 sm:w-[4vw] w-[5vw] lg:w-[1.6vw]" onClick={handleSendMessage} src={send} alt="Send button"/>
                         </div>
                     </div>
                 </div>
