b4fde5d3de1059df89cee18efb066e1fade1c483
ElliotMonde <elliotphua@gmail.com>
2024-07-23 14:58:21 +0800

    added method to mark notifications as read

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -17,13 +17,14 @@ class NotificationsController < ApplicationController
   end
 
   def mark_all_as_read
-    user = params[:id]
+    user = params[:user_id]
+    id = params[:id]
     begin
       @user = User.find(user)
     rescue ActiveRecord::RecordNotFound
       render json: {message: "User does not exist"}, status: :unprocessable_entity
     end
-    @user.notifications.update_all(read: true)
+    Notification.where("user_id": user, "id": id).update_all("read": true)
     render json: {success: true}
   end
 end
