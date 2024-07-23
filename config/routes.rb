8b118cb9fefcf280652d75e4aa46203011fe9270
Yang Si Jun <aersijun@gmail.com>
2024-07-23 17:09:56 +0800

    done chatbot api

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   post 'document', to: 'document#create'
   post 'document/retrieve', to: 'document#retrieve'
   post 'login', to: 'login#create'
+  resources :chatbot
 end
