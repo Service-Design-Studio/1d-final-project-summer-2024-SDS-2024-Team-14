df0585d522c8f914d50086120c8c8b11c99bda8f
Yang Si Jun <aersijun@gmail.com>
2024-07-26 07:05:52 +0800

    done

--- lib/dialogagent.rb
+++ lib/dialogagent.rb
@@ -3,35 +3,43 @@ require 'json'
 
 module Dialogagent
   def get_resp(prompt_text, session_id)
-    base_url = "https://asia-southeast1-dialogflow.googleapis.com/v3/projects/gebirah-14/locations/asia-southeast1/agents/cce15a8e-975a-43a7-876a-f84f942198ac/sessions/#{session_id}:detectIntent"
+    begin
+      base_url = "https://asia-southeast1-dialogflow.googleapis.com/v3/projects/gebirah-14/locations/asia-southeast1/agents/cce15a8e-975a-43a7-876a-f84f942198ac/sessions/#{session_id}:detectIntent"
 
-    stdout, stderr, status = Open3.capture3("gcloud auth print-access-token")
-    access_token = stdout.strip
+      stdout, stderr, status = Open3.capture3("gcloud auth print-access-token")
+      access_token = stdout.strip
 
-    headers = {
-      'Content-Type' => 'application/json; charset=utf-8',
-      'Authorization' => "Bearer #{access_token}",
-      'x-goog-user-project' => 'gebirah-14'
-    }
+      headers = {
+        'Content-Type' => 'application/json; charset=utf-8',
+        'Authorization' => "Bearer #{access_token}",
+        'x-goog-user-project' => 'gebirah-14'
+      }
 
-    body = {
-    queryInput: {
-      text: {
-        text: prompt_text,
+      body = {
+      queryInput: {
+        text: {
+          text: prompt_text,
+        },
+        languageCode: 'en'
       },
-      languageCode: 'en'
-    },
-    queryParams: {
-      timeZone: 'Asia/Singapore'
-    }
-  }.to_json  # Convert body to JSON format
+      queryParams: {
+        timeZone: 'Asia/Singapore'
+      }
+      }.to_json  # Convert body to JSON format
 
-    response = HTTParty.post("#{base_url}", headers: headers, body: body)
-    if response.code == 200
-      parsed_resp = JSON.parse(response.body)
-      resp = parsed_resp["queryResult"]["responseMessages"][0]["text"]["text"][0]
-    else
-      nil
+      response = HTTParty.post("#{base_url}", headers: headers, body: body)
+      if response.code == 200
+        parsed_resp = JSON.parse(response.body)
+        resp = parsed_resp["queryResult"]["responseMessages"][0]["text"]["text"][0]
+      else
+        nil
+      end
     end
+    rescue Errno::ENOENT => e
+      Rails.logger.error("Command not found: #{e.message}")
+      nil
+    rescue => e
+      Rails.logger.error("An error occurred: #{e.message}")
+      nil
   end
 end
