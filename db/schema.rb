b14dc17b596ab4c9abc8203857f8650e6e719ac4
Yang Si Jun <aersijun@gmail.com>
2024-07-26 07:46:21 +0800

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_24_091748) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_23_022123) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -57,7 +57,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_24_091748) do
     t.string "category"
     t.string "content"
     t.boolean "read"
-    t.string "message"
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
