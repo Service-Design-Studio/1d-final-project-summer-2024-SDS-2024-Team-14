f267db3c9ca15b87b0c4f3ae7e0ef06e729eed67
Yang Si Jun <aersijun@gmail.com>
2024-07-26 07:46:21 +0800

    test

--- app/controllers/document_controller.rb
+++ app/controllers/document_controller.rb
@@ -35,50 +35,29 @@ class DocumentController < ApplicationController
       end
   end
 
-  def retrieve
-      user = params[:id]
-      category = params[:category].downcase
-      begin
-          @user = User.find(user)
-      rescue ActiveRecord::RecordNotFound
-          render json: { message: "User does not exist" }, status: :unprocessable_entity and return
-      end
-      @documents = @user.documents.where(category: category)
-      if @documents
-          documents_with_files = @documents.map do |document|
-              {
-                id: document.id,
-                name: document.name,
-                status: document.status,
-                file_url: document.file.attached? ? url_for(document.file) : nil,
-                important: document.important
-              }
-          end
-          render json: {documents: documents_with_files}, status: :ok
-      else
-          render json: {message: "No documents found for this user"}, status: :unprocessable_entity
-      end
-  end
 
-  def status
-    newStatus = params[:status]
-    begin
-      @document = Document.find(params[:id])        
-    rescue ActiveRecord::RecordNotFound
-      render json: {message: "Document does not exist"}, status: :unprocessable_entity and return
+    def retrieve
+        user = params[:id]
+        category = params[:category].downcase
+        begin
+            @user = User.find(user)
+        rescue ActiveRecord::RecordNotFound
+            render json: { message: "User does not exist" }, status: :unprocessable_entity
+        end
+        @documents = @user.documents.where(category: category)
+        if @documents
+            documents_with_files = @documents.map do |document|
+                {
+                  id: document.id,
+                  name: document.name,
+                  status: document.status,
+                  file_url: document.file.attached? ? url_for(document.file) : nil,
+                  important: document.important
+                }
+            end
+            render json: {documents: documents_with_files}, status: :ok
+        else
+            render json: {message: "No documents found for this user"}, status: :unprocessable_entity
+        end
     end
-    if (@document.status != newStatus)
-      @document.update(status: newStatus)
-      if (newStatus == "Approved")
-        NotificationService.document_approved_notification(@document.user_id,@document.name, params[:message])
-      elsif (newStatus == "Rejected")
-        NotificationService.document_rejected_notification(@document.user_id,@document.name, params[:message])
-      end
-      # Rails.logger.info "user id in status: #{@document.name}"
-      render json: {document: @document, message: params[:message]}, status: :ok
-    else
-      # Rails.logger.info "user id in status: #{@document.category}"
-      render json: {message: "Document status is already #{@document.status}. No change applied.", document: @document, optional: params[:message]}, status: :ok
-    end
-  end
 end
