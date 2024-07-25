735ece50fe18c71a65451aec47c6d3a73e5da87b
ciontan <cion.tan.se@gmail.com>
2024-07-25 19:10:37 +0800

    chatbot1 draft 1

---
+++ cypress/support/step_definitions/chatbot/chatbot.cy.js
@@ -0,0 +1,17 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+
+When('I press the enter key', () => {
+    cy.get('#Input').type('{enter}');
+    // Intercept the axios call before triggering the upload button
+    cy.intercept('POST', '/chatbot', (req) => {
+      req.reply({
+        statusCode: 200,
+        fixture: 'chatbot1.json'
+      });
+    }).as('chatbot');
+});
+
+Then(/^I should see a reply "(.+)"$/i, (msg) => {
+    cy.get('#chat-feed').should('contain.text', msg);
+})
