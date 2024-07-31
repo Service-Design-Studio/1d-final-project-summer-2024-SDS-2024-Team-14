f9f02e3e2adf8a48ff956926e5a96c818bf5b17e
Yang Si Jun <aersijun@gmail.com>
2024-08-01 03:53:23 +0800

    finished all missing apis

--- app/serializers/missing_person_serializer.rb
+++ app/serializers/missing_person_serializer.rb
@@ -1,5 +1,5 @@
 class MissingPersonSerializer < ActiveModel::Serializer
-  attributes :name, :age, :gender, :ethnicity, :matched, :date_birth, :matched_user_id
+  attributes :id, :name, :age, :gender, :ethnicity, :matched, :date_birth, :matched_user_id
   def date_birth
     object.date_birth.strftime('%d-%m-%Y') if object.date_birth.present?
   end
