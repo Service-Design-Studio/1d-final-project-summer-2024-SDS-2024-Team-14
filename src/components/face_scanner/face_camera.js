c44d93be91540c3b5142a2ebd4f40c665456fef0
TejaVenket <tejaswiniroyalu2003@gmail.com>
2024-08-03 17:59:59 +0800

    some changes

--- src/components/face_scanner/face_camera.js
+++ src/components/face_scanner/face_camera.js
@@ -18,25 +18,21 @@ export default function CameraView({ cameraRef, setImage }) {
 
     return (
         <div className="relative flex w-full h-7/12 items-center justify-center">
-            {hasAlert && !cameraLoaded? <Alert className="absolute w-10/12 self-center z-40" severity="error"><AlertTitle>No Camera Detected</AlertTitle>Allow camera permissions and refresh page.</Alert> : null}
-            <span className="absolute top-14 z-20 p-1 text-white text-md sm:text-lg xsm:text-xl opacity-55 bg-default bg-opacity-60">Position document within boundary </span>
-            <Image src="/images/A4_crosshairs.svg" className="absolute z-10 w-11/12" width={1} height={1} alt="Camera view" />
-            <Button
-                className="btn-submit absolute z-40 w-fit bottom-5 rounded-full p-1"
-                onMouseEnter={() => setHover("/images/camera_darkblue.svg")}
-                onMouseLeave={() => setHover("/images/camera_white.svg")}
-                onClick={() => {
-                    loadCam();
-                    if (cameraLoaded) {
-                        setImage(cameraRef.current.takePhoto())
-                    } else {
-                        setAlert(true);
-                        setTimeout(() => setAlert(false), 1500);
-                    }
-                }}
-            ><Image src={hover} className="w-fit" width={1} height={1} alt='Category document' /></Button>
-
-            <Camera priority ref={cameraRef} facingMode="environment" aspectRatio={1 / 1.4142} />
+            {hasAlert && !cameraLoaded ? (
+                <Alert className="absolute w-10/12 self-center z-40" severity="error">
+                    <AlertTitle>No Camera Detected</AlertTitle>
+                    Allow camera permissions and refresh page.
+                </Alert>
+            ) : null}
+    
+            <div className="relative w-80 h-80 rounded-full overflow-hidden border-4 border-darkblue shadow-lg">
+                <Camera
+                    priority
+                    ref={cameraRef}
+                    facingMode="environment"
+                    aspectRatio={1} // 1:1 Aspect ratio for the circular view
+                />
+            </div>
         </div>
     )
 }
