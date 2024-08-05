8a07123e36e8fc391096266d6867dbedcce082d4
thamhaowei <95839357+thamhaowei@users.noreply.github.com>
2024-08-05 21:33:09 +0800

    multiple images

--- src/components/Tutorial.js
+++ src/components/Tutorial.js
@@ -7,19 +7,45 @@ import "../styles/globals.css"
 
 const Tutorial = ({ title, steps, onClose }) => {
   const [currentStep, setCurrentStep] = useState(0);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  const handlePrev = () => {
-    if (currentStep > 0) setCurrentStep(currentStep - 1);
+  const handlePrevStep = () => {
+    if (currentStep > 0) {
+      setCurrentStep(currentStep - 1);
+      setCurrentImageIndex(0);
+    }
   };
 
-  const handleNext = () => {
-    if (currentStep < steps.length - 1) setCurrentStep(currentStep + 1);
+  const handleNextStep = () => {
+    if (currentStep < steps.length - 1) {
+      setCurrentStep(currentStep + 1);
+      setCurrentImageIndex(0);
+    }
+  };
+
+  const handlePrevImage = () => {
+    if (currentImageIndex > 0) {
+      setCurrentImageIndex(currentImageIndex - 1);
+    }
+  };
+
+  const handleNextImage = () => {
+    if (currentImageIndex < steps[currentStep].images.length - 1) {
+      setCurrentImageIndex(currentImageIndex + 1);
+    }
   };
 
   const handleClose = () => {
     setCurrentStep(0); // Reset to the first step when closing
-    onClose(); // Call the onClose function to close the modal
+    setCurrentImageIndex(0);
+    if (typeof onClose === 'function') {
+      onClose(); // Call the onClose function to close the modal
+    } else {
+      console.error('onClose is not a function');
+    }
   };
+
+  const currentImages = steps[currentStep].images;
 
   return (
     <div className="fixed inset-0 flex justify-center items-center z-40">
@@ -31,7 +57,10 @@ const Tutorial = ({ title, steps, onClose }) => {
               {steps.map((step, index) => (
                 <li
                   key={index}
-                  onClick={() => setCurrentStep(index)}
+                  onClick={() => {
+                    setCurrentStep(index);
+                    setCurrentImageIndex(0);
+                  }}
                   className={`cursor-pointer p-6 ${currentStep === index ? 'bg-darkblue text-white' : 'text-darkblue'} ${index === 0 ? 'rounded-tl-xl' : ''} ${index !== 0 && 'rounded-none'}`}
                   style={{
                     backgroundColor: currentStep === index ? '#405DB5' : '#F0F4FF',
@@ -49,33 +78,43 @@ const Tutorial = ({ title, steps, onClose }) => {
             <h1 className="block text-3xl mb-8 font-bold text-center text-darkblue">
               {steps[currentStep].title}
             </h1>
-            <div className="flex justify-center items-center h-[40vh] mb-8">
+            <div className="flex justify-center items-center h-[40vh] mb-8 relative">
+              {currentImageIndex > 0 && (
+                <button onClick={handlePrevImage} className="absolute left-0 ml-2 text-darkblue text-2xl">
+                  &#9664;
+                </button>
+              )}
               <Image
-                src={steps[currentStep].image}
+                src={currentImages[currentImageIndex]}
                 alt="Step Image"
                 className="max-h-[40vh] w-auto rounded-lg"
                 width={800}
                 height={600}
               />
+              {currentImageIndex < currentImages.length - 1 && (
+                <button onClick={handleNextImage} className="absolute right-0 mr-2 text-darkblue text-2xl">
+                  &#9654;
+                </button>
+              )}
             </div>
-            <div className="flex justify-center items-center my-0">
-              {steps.map((_, index) => (
+            <div className="flex justify-center items-center my-4">
+              {currentImages.length > 1 && currentImages.map((_, index) => (
                 <span
                   key={index}
-                  onClick={() => setCurrentStep(index)}
-                  className={`h-2 w-2 rounded-full mx-1 cursor-pointer ${index === currentStep ? 'bg-darkblue' : 'bg-gray-300'}`} style={{ backgroundColor: index !== currentStep ? '#E0E0E0' : '' }}
+                  onClick={() => setCurrentImageIndex(index)}
+                  className={`h-2 w-2 rounded-full mx-1 cursor-pointer ${index === currentImageIndex ? 'bg-darkblue' : 'bg-gray-300'}`} style={{ backgroundColor: index !== currentImageIndex ? '#E0E0E0' : '' }}
                 />
               ))}
             </div>
             <div className="flex-grow flex items-center justify-center">
-              <p className="text-darkblue text-[1.3rem] mb-12 px-20 text-center leading-tight max-h-[13vh] overflow-auto">
+              <p className="text-darkblue text-xl mb-12 px-20 text-center leading-tight max-h-[13vh] overflow-auto">
                 {steps[currentStep].text}
               </p>
             </div>
             <div className="flex justify-between mt-4">
               {currentStep > 0 && (
                 <button
-                  onClick={handlePrev}
+                  onClick={handlePrevStep}
                   className="bg-transparent border-none cursor-pointer text-[1em] text-darkblue flex items-center"
                 >
                   <span className="mr-1 text-lg">&larr;</span>
@@ -84,7 +123,7 @@ const Tutorial = ({ title, steps, onClose }) => {
               )}
               {currentStep < steps.length - 1 && (
                 <button
-                  onClick={handleNext}
+                  onClick={handleNextStep}
                   className="bg-transparent border-none cursor-pointer text-[1rem] text-darkblue flex items-center ml-auto"
                 >
                   Next <span className="ml-1 text-lg">&rarr;</span>
@@ -104,11 +143,11 @@ Tutorial.propTypes = {
     PropTypes.shape({
       category: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
-      image: PropTypes.string.isRequired,
+      images: PropTypes.arrayOf(PropTypes.string).isRequired,
       text: PropTypes.string.isRequired,
     })
   ).isRequired,
-  onClose: PropTypes.func.isRequired,
+  onClose: PropTypes.func.isRequired, // Ensure this is defined
 };
 
 export default Tutorial;
--- src/components/modalContent/famtree.js
+++ src/components/modalContent/famtree.js
@@ -1,26 +1,28 @@
 // src/components/modalContent/famtree.js
 import React from 'react';
-import CreateNewSearchImage from 'public/images/intro/homehd.png';
-import ViewExistingSearchImage from 'public/images/intro/homehd.png';
-import EditExistingSearchImage from 'public/images/intro/homehd.png';
+import CreateNewSearchImage1 from 'public/images/intro/homehd.png';
+import CreateNewSearchImage2 from 'public/images/intro/docmanfigmahd.png';
+import ViewExistingSearchImage from 'public/images/intro/white bg.png';
+import EditExistingSearchImage1 from 'public/images/intro/docmanhd.png';
+import EditExistingSearchImage2 from 'public/images/intro/homehd.png';
 
 const famTreeContent = [
     {
         category: 'Create New Search',
         title: 'Create New Search',
-        image: CreateNewSearchImage,
+        images: [CreateNewSearchImage1, CreateNewSearchImage2],
         text: 'This section allows you to create a new search for your family members. Fill in the required details and click on the "Search" button to begin the process.',
     },
     {
         category: 'View Existing Search',
         title: 'View Existing Search',
-        image: ViewExistingSearchImage,
+        images: [ViewExistingSearchImage],
         text: 'Here you can view the details of your existing searches. You can see the search status and other relevant information.',
     },
     {
         category: 'Edit Existing Search',
         title: 'Edit Existing Search',
-        image: EditExistingSearchImage,
+        images: [EditExistingSearchImage1, EditExistingSearchImage2],
         text: 'If you need to make changes to an existing search, you can do so in this section. Update the details as necessary and save your changes.',
     },
 ];
