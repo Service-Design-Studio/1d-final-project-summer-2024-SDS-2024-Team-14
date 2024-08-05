4be7e2af0dfb15ef934c5884507833e91ea0dfd1
ciontan <cion.tan.se@gmail.com>
2024-08-06 03:12:10 +0800

    loading animation

--- src/components/loading.js
+++ src/components/loading.js
@@ -3,7 +3,7 @@ import '../styles/globals.css';
 
 export default function Loading() {
     return (
-        <div className="z-100 flex w-full h-screen items-center justify-center bg-white">
+        <div className="flex w-full h-screen items-center justify-center bg-white">
             <div className='w-100 h-fit relative animate-pulse'>
                 <Image width={100} height={100} className='w-full h-auto py-3' src={"/images/enable_id_logo.svg"} alt="Login loading" />
                 <span className="text-2xl text-center font-medium text-darkblue">
