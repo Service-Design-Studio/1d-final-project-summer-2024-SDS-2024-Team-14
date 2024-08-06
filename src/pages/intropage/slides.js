a287d41fefa6b7a25ae9c8c424e86aad7b457b27
thamhaowei <95839357+thamhaowei@users.noreply.github.com>
2024-08-06 11:21:17 +0800

    intropage responsiveness done

--- src/pages/intropage/slides.js
+++ src/pages/intropage/slides.js
@@ -1,31 +1,32 @@
 // src/pages/intropage/slides.js
 const slides = [
     {
-        image: '/images/intro/slide1.jpg',
+        imageLg: '/images/intro/slide1.jpg',
+        imageSm: '/images/intro/phoneref.jpg',
         title: 'Introduction',
         text: 'Welcome to Enable ID, a platform supporting refugees with secure digital identity cards for accessing employment, education, and healthcare.'
     },
     {
-        image: '/images/intro/homehd.png',
-        // homefigma.jpg
+        imageLg: '/images/intro/homehd.png',
+        imageSm: '/images/intro/phonehomehd.png',
         title: 'Homepage',
         text: 'This is our homepage, where you can access the different services as well as view your digital identity card.'
     },
     {
-        image: '/images/intro/famtree.png',
-        // if image not loading, change to fam.jpg
+        imageLg: '/images/intro/famtree.png',
+        imageSm: '/images/intro/phonefamtreefull.png',
         title: 'Family Tree',
         text: 'Our family tree page aims to assist users in locating missing family members and friends.'
     },
     {
-        image: '/images/intro/chatbothd.png',
-        // if image not loading, change to chatbot.jpg
+        imageLg: '/images/intro/chatbothd.png',
+        imageSm: '/images/intro/chatbothd.png',
         title: 'FAQ Chatbot',
         text: 'If you have any questions regarding our webapp, our FAQ Chatbot will attempt to answer them and aid in re-navigating you to your desired page. Click on the icon at the bottom right of every page to open it.'
     },
     {
-        image: '/images/intro/docmanhd.png',
-        // docmanfigma.jpg
+        imageLg: '/images/intro/docmanhd.png',
+        imageSm: '/images/intro/phonedocman.png',
         title: 'Documents Manager',
         text: 'On the Documents Manager page, you will be able to view and access all your previously uploaded documents. You can also check on their approval status.'
     },
