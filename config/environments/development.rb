47545b427a7c65eabaa84181b5f46a47e95d7a62
Yang Si Jun <aersijun@gmail.com>
2024-08-06 21:47:54 +0800

    final changes to api

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+  # For getting url of image served by rails server
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
